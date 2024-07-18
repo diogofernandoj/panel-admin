0e7ced131bcc4bec93a0ea83c1c8589b6a20bbc2
Diogo Jorge <diogofernandoj@gmail.com>
2024-07-18 12:08:56 -0300

    feat: add TransactionReports component

---
+++ app/(private-routes)/dashboard/_components/transaction-reports.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import React from 'react'
+
+import { Pie, PieChart } from 'recharts'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/app/_components/ui/card'
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+} from '@/app/_components/ui/chart'
+const chartData = [
+  { transaction: 'earnings', transactions: 275, fill: 'var(--color-earnings)' },
+  { transaction: 'expenses', transactions: 200, fill: 'var(--color-expenses)' },
+  { transaction: 'balance', transactions: 187, fill: 'var(--color-balance)' },
+]
+
+const chartConfig = {
+  transactions: {
+    label: 'Transactions',
+  },
+  earnings: {
+    label: 'Entradas',
+    color: '#22c55e',
+  },
+  expenses: {
+    label: 'Saídas',
+    color: '#ef4444',
+  },
+  balance: {
+    label: 'Balanço',
+    color: '#3b82f6',
+  },
+} satisfies ChartConfig
+
+const TransactionReports = () => {
+  return (
+    <Card className="flex flex-col bg-slate-900 w-full">
+      <CardHeader className="items-center pb-0">
+        <CardTitle className="text-sm">Transaction Reports</CardTitle>
+        <CardDescription className="text-xs">
+          January - June 2024
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square max-h-[300px]"
+        >
+          <PieChart>
+            <Pie data={chartData} dataKey="transactions" />
+            <ChartLegend
+              content={<ChartLegendContent nameKey="transaction" />}
+              className="-translate-y-2 flex-wrap gap-2 [&>*]:basis-1/4 [&>*]:justify-center"
+            />
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default TransactionReports
--- app/(private-routes)/dashboard/page.tsx
+++ app/(private-routes)/dashboard/page.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import DashboardHeader from './_components/dashboard-header'
 import Cards from './_components/cards'
+import TransactionReports from './_components/transaction-reports'
 
 const Dashboard = async () => {
   return (
     <div className="flex flex-col w-full p-8">
       <DashboardHeader />
-      <div className="flex mt-8">
+      <div className="flex mt-8 items-start gap-8">
         <Cards />
+        <TransactionReports />
       </div>
     </div>
   )
