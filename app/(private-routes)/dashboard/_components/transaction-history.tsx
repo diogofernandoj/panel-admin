f74f3c5d8436d376c754cd5a8968dacc44f3d59c
Diogo Jorge <diogofernandoj@gmail.com>
2024-07-19 10:39:49 -0300

    style: add card to TransactionHistory

--- app/(private-routes)/dashboard/_components/transaction-history.tsx
+++ app/(private-routes)/dashboard/_components/transaction-history.tsx
@@ -16,6 +16,7 @@ import { format } from 'date-fns'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/app/_lib/auth'
 import { makeCurrencyNumber } from '@/app/_lib/utils'
+import { Card } from '@/app/_components/ui/card'
 
 const TransactionHistory = async () => {
   const session = await getServerSession(authOptions)
@@ -27,9 +28,9 @@ const TransactionHistory = async () => {
   const transactions: Transaction[] = await db.transaction.findMany({
     where: {
       userId: session.user.id,
-      date: new Date(
-        new Date(new Date().setUTCHours(-3, 0, 0, 0)).setUTCHours(0)
-      ),
+      // date: new Date(
+      //   new Date(new Date().setUTCHours(-3, 0, 0, 0)).setUTCHours(0)
+      // ),
     },
   })
 
@@ -41,49 +42,51 @@ const TransactionHistory = async () => {
   )
 
   return (
-    <Table className="">
-      <TableHeader>
-        <TableRow>
-          <TableHead></TableHead>
-          <TableHead>Data</TableHead>
-          <TableHead>Título</TableHead>
-          <TableHead className="text-right">Valor</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {data.map((item) => (
-          <TableRow key={item.id}>
-            <TableCell>
-              {item.type ? (
-                <CircleArrowUpIcon className="text-green-400" size={18} />
-              ) : (
-                <CircleArrowDownIcon className="text-red-400" size={18} />
-              )}
-            </TableCell>
-            <TableCell className="font-medium">
-              {format(
-                new Date(new Date(item.date).setUTCHours(3)),
-                'dd-MM-yyyy'
-              )}
-            </TableCell>
-            <TableCell>{item.title}</TableCell>
-            <TableCell
-              className={`text-right ${item.type ? 'text-green-400' : 'text-red-400'}`}
-            >{`${item.type ? '+' : '-'}${makeCurrencyNumber(Number(item.amount))}`}</TableCell>
+    <Card>
+      <Table className="">
+        <TableHeader>
+          <TableRow>
+            <TableHead></TableHead>
+            <TableHead>Data</TableHead>
+            <TableHead>Título</TableHead>
+            <TableHead className="text-right">Valor</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-      <TableFooter>
-        <TableRow>
-          <TableCell colSpan={3}>Total</TableCell>
-          <TableCell className="text-right">
-            <span className="bg-blue-500 bg-opacity-10 w-max py-2 px-4 rounded-full text-blue-500">
-              {makeCurrencyNumber(totalAmount)}
-            </span>
-          </TableCell>
-        </TableRow>
-      </TableFooter>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {data.map((item) => (
+            <TableRow key={item.id}>
+              <TableCell>
+                {item.type ? (
+                  <CircleArrowUpIcon className="text-green-400" size={18} />
+                ) : (
+                  <CircleArrowDownIcon className="text-red-400" size={18} />
+                )}
+              </TableCell>
+              <TableCell className="font-medium">
+                {format(
+                  new Date(new Date(item.date).setUTCHours(3)),
+                  'dd-MM-yyyy'
+                )}
+              </TableCell>
+              <TableCell>{item.title}</TableCell>
+              <TableCell
+                className={`text-right ${item.type ? 'text-green-400' : 'text-red-400'}`}
+              >{`${item.type ? '+' : '-'}${makeCurrencyNumber(Number(item.amount))}`}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={3}>Total</TableCell>
+            <TableCell className="text-right">
+              <span className="bg-blue-500 bg-opacity-10 w-max py-2 px-4 rounded-full text-blue-500">
+                {makeCurrencyNumber(totalAmount)}
+              </span>
+            </TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </Card>
   )
 }
 
