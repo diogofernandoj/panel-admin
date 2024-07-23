ef6bebef18e04b25e0d86374f27883b27bf72198
Diogo Jorge <diogofernandoj@gmail.com>
2024-07-23 13:26:50 -0300

    feat: add DashboardContent component

---
+++ app/(private-routes)/dashboard/_components/dashboard-content.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+
+import Cards from './cards'
+import TransactionBarChart from './transaction-bar-chart'
+import TransactionPieChart from './transaction-pie-chart'
+import { Transaction } from '@prisma/client'
+import { getFilteredTransactions, Period } from '@/app/_lib/utils'
+import TransactionHistory from './transaction-history'
+
+const DashboardContent = ({
+  transactions,
+}: {
+  transactions: Transaction[]
+}) => {
+  const [period, setPeriod] = useState<Period>('weekly')
+  const [filteredTransactions, setFilteredTransactions] = useState<
+    Transaction[] | null
+  >(null)
+
+  useEffect(() => {
+    const filterTransactions = () => {
+      setFilteredTransactions(getFilteredTransactions(transactions, period))
+    }
+    filterTransactions()
+  }, [period])
+
+  if (!filteredTransactions) return null
+
+  return (
+    <div className="flex flex-col gap-6">
+      <Cards transactions={filteredTransactions} />
+      <div className="flex gap-6">
+        <div className="flex flex-col gap-6 w-full">
+          <TransactionBarChart
+            transactions={filteredTransactions}
+            period={period}
+            setPeriod={setPeriod}
+          />
+          <TransactionHistory transactions={filteredTransactions} />
+        </div>
+        <div className="flex flex-col gap-6 w-1/3">
+          <TransactionPieChart />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DashboardContent
--- app/(private-routes)/dashboard/page.tsx
+++ app/(private-routes)/dashboard/page.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import Cards from './_components/cards'
-import TransactionHistory from './_components/transaction-history'
-import TransactionLineChart from './_components/transaction-line-chart'
-import TransactionPieChart from './_components/transaction-pie-chart'
 import DashboardHeader from './_components/dashboard-header'
-import TransactionBarChart from './_components/transaction-bar-chart'
 import { db } from '@/app/_lib/prisma'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/app/_lib/auth'
+import DashboardContent from './_components/dashboard-content'
 
 const Dashboard = async () => {
   const session = await getServerSession(authOptions)
@@ -20,17 +16,7 @@ const Dashboard = async () => {
   return (
     <div className="flex flex-col w-full p-6 gap-6">
       <DashboardHeader />
-      <Cards transactions={transactions} />
-      <div className="flex gap-6">
-        <div className="flex flex-col gap-6 w-full">
-          <TransactionBarChart transactions={transactions} />
-          <TransactionHistory />
-        </div>
-        <div className="flex flex-col gap-6">
-          <TransactionPieChart />
-          <TransactionLineChart />
-        </div>
-      </div>
+      <DashboardContent transactions={transactions} />
     </div>
   )
 }
