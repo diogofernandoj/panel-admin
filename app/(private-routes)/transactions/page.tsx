95b22af0429c88232d8f0470bc7cba9915deb543
Diogo Jorge <diogofernandoj@gmail.com>
2024-07-28 15:22:37 -0300

    feat: ensure transaction is sorted by date

--- app/(private-routes)/transactions/page.tsx
+++ app/(private-routes)/transactions/page.tsx
@@ -14,10 +14,14 @@ const TransactionsPage = async () => {
     },
   })
 
+  const sortedTransactions = transactions.sort(
+    (a, b) => Number(b.date) - Number(a.date)
+  )
+
   return (
     <div className="min-h-screen p-6 flex flex-col gap-6">
       <DashboardHeader title="Transações" />
-      <DataTable columns={columns} data={transactions} />
+      <DataTable columns={columns} data={sortedTransactions} />
     </div>
   )
 }
