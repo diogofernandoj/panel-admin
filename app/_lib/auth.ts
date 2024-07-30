2e3f4bcb1210c09911a2e55e5db5aa88b8364aaa
Diogo Jorge <diogofernandoj@gmail.com>
2024-07-30 08:47:47 -0300

    chore: add initial Auth@v5 setup

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,27 +1,10 @@
-import { db } from '@/app/_lib/prisma'
+import NextAuth from 'next-auth'
+import Google from 'next-auth/providers/google'
 import { PrismaAdapter } from '@auth/prisma-adapter'
-import { AuthOptions } from 'next-auth'
-import { Adapter } from 'next-auth/adapters'
-import GoogleProvider from 'next-auth/providers/google'
+import { db } from './prisma'
 
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(db) as Adapter,
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  callbacks: {
-    async session({ session, user }) {
-      session.user = { ...session.user, id: user.id } as {
-        id: string
-        name: string
-        email: string
-      }
-
-      return session
-    },
-  },
-  secret: process.env.NEXT_AUTH_SECRET,
-}
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  providers: [Google],
+  adapter: PrismaAdapter(db),
+  session: { strategy: 'jwt' },
+})
